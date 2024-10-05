b03bbe92307f6078f4d4f7ae907868b34df1cc6a
BamanGirma <amanuelgirma360@gmail.com>
2024-10-05 11:35:11 +0300

    axios configuration

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
-
+import axios from "axios";
+const baseURL = import.meta.env.VITE_BASE_URL;
 const axiosBase = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: baseURL,
 });
 
-export default axiosBase
+export default axiosBase;
